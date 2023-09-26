f65c143f2797d78a1862d01550db8975ea840dce
mateuszskrzynsky <mateusz.skrzynsky@outlook.com>
2023-09-26 19:03:18 +0200

    Added Regex001

--- src/regex/Regex001.java
+++ src/regex/Regex001.java
@@ -8,26 +8,17 @@ Napisz Regex, który sprawdza polskie numery telefonów komórkowych,
 zaczynające się cyfrą 5.
  */
 public class Regex001 {
-    private static final Pattern PATTERN = Pattern.compile("(\\+48)?5[0-9]{8}");
-
     public static void main(String[] args) {
-      validate("+48515123456");
-      validate("+48715487589");
-        validate("515332123");
-        validate("515336293");
-        validate("415336293");
-        validate("615332123");
-        validate("715332123");
-        validate("712123");
-        validate("51123");
+        RegexTester tester = new RegexTester("(\\+48)?5[0-9]{8}");
+      tester.validate("+48515123456");
+      tester.validate("+48715487589");
+        tester.validate("515332123");
+        tester.validate("515336293");
+        tester.validate("415336293");
+        tester.validate("615332123");
+        tester.validate("715332123");
+        tester.validate("712123");
+        tester.validate("51123");
     }
 
-    public static void validate(String text){
-        Matcher matcher = PATTERN.matcher(text);
-        if(matcher.matches()){
-            System.out.println("Prawda dla tekstu: " + text);
-        } else {
-            System.out.println("Fałsz dla tekstu: " + text);
-        }
-    }
 }
